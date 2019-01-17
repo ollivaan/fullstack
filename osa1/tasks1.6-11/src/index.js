bd14d5448f069a4c3dac655c33325389b6609e42
Ollipekka Väänänen <Ollipekka.vaananen@helsinki.fi>
2019-01-18 00:42:22 +0200

    tehtävät 7-10

--- osa1/tasks1.6-11/src/index.js
+++ osa1/tasks1.6-11/src/index.js
@@ -8,12 +8,53 @@ const Button = ({handleClick, text}) => (
 
 )
 const Statistic = (props) => {
+    console.log(props)
     return (
         <div>
             <p>Good: {props.good}</p>
             <p>Neutral: {props.neutral}</p>
             <p>Bad: {props.bad}</p>
+            <Sum good={props.good} neutral={props.neutral} bad={props.bad} />
+            <Ka good={props.good} neutral={props.neutral} bad={props.bad} />
+            <PositivesOnly good={props.good} neutral={props.neutral} bad={props.bad} />
+
         </div>
+    )
+}
+const Sum = (props) => {
+    return (
+        <div>
+            <p>Sum: {props.good + props.neutral + props.bad}</p>
+        </div>
+    )
+}
+const Ka = (props) => {
+    return ( 
+        <div>
+            <p>Ka: {(props.good - props.bad) / (props.good + props.neutral + props.bad)} </p>
+        </div>
+    )
+}
+const PositivesOnly = (props) => {
+    return (
+        <div>
+            <p> Positives: {(props.good)/(props.good + props.neutral + props.bad)*100} %</p>
+        </div>
+    )
+}
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+      return (
+        <div>
+            Painele nappuloita
+        </div>
+      )
+    }
+  
+    return (
+      <div>
+        <Statistic good={props.good} neutral={props.neutral} bad={props.bad}/>
+      </div>
     )
 }
 const App = () => {
@@ -21,27 +62,34 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState([])
   
   const handleGoodClick = () => {
+    setAll(allClicks + 1)
     setGood(good + 1)
   }
   const handleNeutralClick = () => {
+    setAll(allClicks + 1)
     setNeutral(neutral + 1)
   }
   const handleBadClick = () => {
+    setAll(allClicks + 1)  
     setBad(bad + 1)
   }  
   return (
     <div>
         <h1>Give feedback</h1>
         {/* {good} */}
+
         <Button handleClick={handleGoodClick} text='Good'/>
         {/* {neutral} */}
         <Button handleClick={handleNeutralClick} text='Neutral'/>
         {/* {bad} */}
         <Button handleClick={handleBadClick} text='Bad'/>
         <h2>Statistiikka</h2>
-        <Statistic good={good} neutral={neutral} bad={bad}/>
+        <History allClicks={allClicks} good={good} neutral={neutral} bad={bad} />
+        
+
     </div>
   )
 }
