a31f03fa462696108787be8d1ca28f94be58a96a
Ollipekka Väänänen <Ollipekka.vaananen@helsinki.fi>
2019-01-28 17:49:28 +0200

    tehtävä 2.4 tehty

--- osa2/osa2.1-5/src/index.js
+++ osa2/osa2.1-5/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = props =>
-  <h1>{props.course}</h1>
+const Header = props => {
+
+  return (
+  <h1> {props.courses}</h1>
+  )
+  
+} 
+
+ 
 
 const Total = props => {
   const pituus = props.parts.length
@@ -13,9 +20,6 @@ const Total = props => {
   }
   const reducer = (accumulator, currentValue) => accumulator + currentValue;
   const total = array.reduce(reducer)
-
-
-
 
 return <p>yhteensä {total} tehtävää</p>
 }
@@ -34,43 +38,68 @@ const Content = ({parts}) => parts.map(part =>
       exercises={part.exercises}
     />
   )
-const Course = props => {
-    const { course } = props
+const Courses = props => {
+    const { courses } = props
+
     return (
     <div>
-    <Header course={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts} />
+    <h1>Opetusohjelma</h1>    
+    <Header courses={courses[0].name} />
+    <Content parts={courses[0].parts} />
+    <Total parts={courses[0].parts} />
+
+    <Header courses={courses[1].name} />
+    <Content parts={courses[1].parts} />
+    <Total parts={courses[1].parts} />
 
     </div>
     )
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack -sovelluskehitys',
-    parts: [
-      {
-        name: 'Reactin perusteet',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Tiedonvälitys propseilla',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'Komponenttien tila',
-        exercises: 2,
-        id: 3
-      }      
-    ]
-  } 
+   const courses = [
+    {
+      name: 'Half Stack -sovelluskehitys',
+      id: 1,
+      parts: [
+        {
+          name: 'Reactin perusteet',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Tiedonvälitys propseilla',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'Komponenttien tila',
+          exercises: 14,
+          id: 3
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 2,
+          id: 1
+        },
+        {
+          name: 'Middlewaret',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course} />
+      <Courses courses={courses} />
     </div>
   )
 }
