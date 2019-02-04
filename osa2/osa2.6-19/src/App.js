b9b0c539471d55843e9daa7d6a06dde7d9319c58
Ollipekka Väänänen <Ollipekka.vaananen@helsinki.fi>
2019-02-04 22:07:23 +0200

    muutettu kansion nimeä

---
+++ osa2/osa2.6-19/src/App.js
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from 'react'
+import personService from './services/persons'
+
+const Persons = ({persons}) => {
+    const result = persons.map(person => <li key={Math.random()}>{person.name} {person.number}</li>)
+        return (
+        <div>{result} </div>
+        ) 
+}
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+  return (
+    <div className="error">
+      {message}
+    </div>
+  )
+}
+const PersonForm = props => (
+    <form onSubmit={props.addPerson}>
+      <div>
+        nimi: 
+        <input
+          value={props.newName}
+          onChange={props.handlePersonChange}
+        />
+      </div>
+      <div>
+        numero:
+        <input
+          value={props.newNumber}
+          onChange={props.handleNumberChange}
+        />
+      </div>
+      <div>
+        <button type="submit">lisää</button>
+      </div>
+    </form>
+  )
+const App = () => {
+  const [errorMessage, setErrorMessage] = useState('')
+  const [ persons, setPersons] = useState([
+  ]) 
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber] = useState('')
+
+
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+  
+  const handlePersonChange = (event) => {
+
+    setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+
+    setNewNumber(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      number: newNumber
+
+    }
+
+    const result = persons.map(person => person.name)
+ 
+    if (result.indexOf(personObject.name) === -1) {
+        personService
+        .create(personObject)
+        .then(response => {
+          setPersons(persons.concat(response.data))
+          setNewName('')
+          setNewNumber('')
+        })
+   
+      
+      } else { 
+        setErrorMessage(
+          `${newName} on jo luettelossa`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000) 
+      } 
+
+  }
+  return (
+    <div>
+      <h2>Puhelinluettelo</h2>
+      <h3>lisää uusi</h3>
+      <PersonForm 
+        addPerson={addPerson}
+        newName={newName}
+        handlePersonChange={handlePersonChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+     
+      />        
+      <h3>Numerot</h3>
+      <Notification message={errorMessage} />
+      <Persons
+        persons={persons}
+
+        />
+    </div>
+  )
+
+}
+
+export default App
